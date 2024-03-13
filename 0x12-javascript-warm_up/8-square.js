02c37d1f19d6563b50db428462b0a598f9e8e422
iamajirobamusty <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-13 15:53:53 +0100

    Patterns

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+let myVar = "X";
+if (!argv[2] || !parseInt(argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let itr = argv[2]; itr > 1; itr--) {
+    myVar += "X";
+  }
+  while (argv[2]) {
+    console.log(myVar);
+    argv[2]--;
+  }
+}
