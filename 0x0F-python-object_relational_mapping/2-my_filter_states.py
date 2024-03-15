e9510a3ad9521c88d538d0d27294cdb307aa0b31
musty <ajiroba980@gmail.com>
2024-03-15 04:07:04 -0700

    corrections

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ from sys import argv
 if __name__ == "__main__":
   db_connect = MySQLdb.connect(host='localhost', prot=3306, user=argv[1], password=argv[2], db=argv[3])
   db_cursor = db_connect.cursor()
-  db_cusor.execute("SELECT * FORM states WHERE name LIKE BINARY '{}' ORDER BY=states.id ASC".format(argv[4]))
+  db_cusor.execute("SELECT * FORM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4]))
   result = db_cursor.fetchall()
   for col in result:
     print(col)
