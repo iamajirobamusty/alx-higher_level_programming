3641efc22c6ba1994f8a05b9d553b7ac0c4a999a
iamajirobamusty <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-15 17:14:35 +0100

    Name error issue corrected

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,10 +10,10 @@ from sys import argv
 
 if __name__ == "__main__":
     db_connect = MySQLdb.connect(host='localhost', prot=3306, user=argv[1], password=argv[2], db=argv[3])
-    db_cursor = db_connect.cursor()
-    db_cusor.execute("SELECT * FORM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
-    result = db_cursor.fetchall()
+    Cur = db_connect.cursor()
+    Cur.execute("SELECT * FORM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    result = Cur.fetchall()
     for col in result:
         print(col)
-    db_connect.close()
+    Cur.close()
     db_connect.close()
