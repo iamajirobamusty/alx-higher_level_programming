40b37e06ebf323503fa2a45583a0956790da1089
musty <ajiroba980@gmail.com>
2024-03-15 02:39:59 -0700

    permissions

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-  db_connect = MySQLdb.connect(host='localhost', prot=3306, user=argv[1], password=argv[2], db=argv[3])
+  db_connect = MySQLdb.connect(host='localhost', port=3306, user=argv[1], password=argv[2], db=argv[3])
   db_cursor = db.cursor()
   db_cusor.execute("SELECT * FORM states ORDER BY=states.id")
   result = db_cursor.fetchall()
