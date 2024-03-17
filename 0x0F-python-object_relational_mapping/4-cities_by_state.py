559f8fc64bd97e9f52d038536a1f94d2af20c3f7
iamajirobamusty <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-17 08:47:22 +0100

    Correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,9 +8,9 @@ from sys import argv
 
 if __name__ == "__main__":
     db_connection = db.connect(host='localhost', port=3306, user=argv[1], password=argv[2], db=argv[3])
-    db_cursor = db_connection.cursor()
-    db_cursor.execute("SELECT cities.id, cities.name, states.name FROM JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"%(argv[4]))
-    rows_selected = db_cursor.fetchall()
+    Cur = db_connection.cursor()
+    Cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC)
+    result = Cur.fetchall()
 
-    for row in rows_selected:
+    for row in result:
         print("row")
