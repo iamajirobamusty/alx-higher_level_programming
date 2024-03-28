1096da1dc0823ae9d485ab5847250f57ca3cb8c5
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-28 22:05:32 +0100

    Python requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status.
+"""
+
+import requests
+
+if __name__ == "__main__":
+  r = requests.get('https://alx-intranet.hbtn.io/status')
+  print("""
+        - type: {}$,
+        - conent: {}$
+        """.format(type(r.text), r.text)
+        )
