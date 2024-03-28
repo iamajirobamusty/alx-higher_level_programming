77f8d3c83710c60e42a28a9ea7c31573d05b687e
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-28 22:09:44 +0100

    Requests headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# A script that get the X-Request-Id using python requests module
+import requests
+import sys
+
+if __name__ == "__main__":
+  url = sys.argv[1]
+  r = requests.get(url)
+  print(r.headers['X-Request-Id'])
