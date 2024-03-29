487694085bffc9e9349dc740bae81027b2c1a50d
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-29 14:55:20 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,11 +2,10 @@
 """
 script that fetches https://alx-intranet.hbtn.io/status.
 """
-
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("""
           Body response:
           - type: {}$,
