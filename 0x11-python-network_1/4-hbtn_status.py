12408e2ec92b32edcb9801797deb68f6ae7273b3
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-29 14:57:00 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,9 +6,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
-    print("""
-          Body response:
-          - type: {}$,
-          - conent: {}$
-          """.format(type(r.text), r.text)
-         )
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
