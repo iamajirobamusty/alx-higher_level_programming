c54f9fd6f2a0fe89889051abcb28caaba0e12967
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-29 14:36:49 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -20,6 +20,5 @@ if __name__ == "__main__":
             print(html)
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
-    except URLError as e:
-        print(e.reason)
+    
   
