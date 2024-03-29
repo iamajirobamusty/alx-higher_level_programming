40ae40c833fe5ffa07f0b1eb3d3217501ab46aab
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-29 16:03:29 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,6 +2,8 @@
 """
 script that takes in a URL and an email, sends a POST request to the passed
 URL with the email as a parameter, and displays the body of the response.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
 import requests
 import sys
