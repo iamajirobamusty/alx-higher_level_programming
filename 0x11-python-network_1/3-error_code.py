6be0313dc7cf49068b8308b8e72a0b26aae12f4d
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-03-29 15:20:23 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,7 +6,7 @@ You have to manage urllib.error.HTTPError exceptions and
 print: Error code: followed by the HTTP status code.
 """
 import urllib.request
-import urllib.error
+from urllib.error import HTTPError
 import sys
 
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("utf-8"))
-    except urllib.error.HTTPError as e:
+    except HTTPError as e:
         print("Error code: {}".format(e.code)))
     
   
