e9ed5e346133bf31656b0cba7b2ffca034deca9b
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-05-02 09:35:22 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const { argv } = require('node:process');
 if (parseInt(argv[2])) {
-  console.log(`${argv[2]}`);
+  console.log(`My number: ${argv[2]}`);
 } else {
   console.log('Not a number');
 }
